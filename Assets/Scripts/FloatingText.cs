b57fd5661c318e768585a1733f4eea21d357ed8d
JimmyCelly <Jacelly@espol.edu.ec>
2020-01-19 18:00:00 -0500

    Agregando texto guia que desaparece por un tiempo dado, en el nivel 2

---
+++ Assets/Scripts/FloatingText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+    public float My_TimeLife = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        My_TimeLife -= 0.03f;
+        if (My_TimeLife <= 0)
+        {
+          Destroy(this.gameObject);
+        }
+    }
+}
